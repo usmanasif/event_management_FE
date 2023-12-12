0cbe91974296c3d4c3c5a5c5b6e955d93cbf5bba
Sam Asif <sam.asif.ca@gmail.com>
2023-12-11 22:49:32 -0500

    Added netlify deployment

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 import EventList from '../components/EventList';
 import CreateEvent from "../components/CreateEvent";
 import JoinedEvents from '../components/JoinedEvents';
 import JoinEvent from '../components/JoinEvent';
+import { useEvent } from "../context/EventContext";
 
 const Home = () => {
-  return <>
-    <EventList />
-    <CreateEvent />
-    <JoinedEvents />
-    <JoinEvent />
-  </>;
+  const { state } = useEvent();
+  const { isAuthenticated } = state;
+  
+  return (
+    <>
+      {isAuthenticated && <EventList />}
+      {isAuthenticated && <CreateEvent />}
+      {isAuthenticated && <JoinedEvents />}
+      {isAuthenticated && <JoinEvent />}
+    </>
+  );
 };
 
 export default Home;
